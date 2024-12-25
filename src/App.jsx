54fe31ce082ce34fd3378faadf05d697a7923923
SanadSuleman <110310084+sanadsul@users.noreply.github.com>
2024-12-25 20:42:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import Signup from "./components/signup";
+import Signup from "./components/Signup";
 import Footer from "./components/Footer";
 import Profile from "./components/Profile";
 import Pub from "./components/Pub";
