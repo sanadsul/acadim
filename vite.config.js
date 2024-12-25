460ab5542320c2258ee00eace423fa1cc2f8d41d
SanadSuleman <110310084+sanadsul@users.noreply.github.com>
2024-12-25 20:37:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { commonjs } from '@vitejs/plugin-commonjs'; // استيراد إضافة commonjs
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ include: /\.(mdx|js|jsx|ts|tsx)$/ })],
+  plugins: [
+    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
+    commonjs() // إضافة إضافة commonjs هنا
+  ],
   server: {
     host: "0.0.0.0",
     port: 5173,
