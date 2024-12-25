3252c75f2e2c36cb9e4d251b177e4daac393219a
SanadSuleman <110310084+sanadsul@users.noreply.github.com>
2024-12-25 20:34:42 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
-const flowbite = require("flowbite-react/tailwind");
+// تأكد من المسار الصحيح بناءً على هيكل مكتبة flowbite-react
+import flowbite from 'flowbite-react/tailwind';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin(),],
-};
+  plugins: [
+    flowbite
+  ],
+}
